79d4908fb34f1f085f15b3319bab8149c3e8a775
imstar15 <imstar15@qq.com>
2021-07-28 19:31:00 +0800

    Discord faucet robot

--- src/bot/discord.js
+++ src/bot/discord.js
@@ -1,14 +1,64 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const axios = require('axios');
+const pdKeyring = require('@polkadot/keyring');
 
+let ax = axios.create({
+  baseURL: process.env.BACKEND_URL,
+  timeout: 10000,
+});
+
+const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  let [action, arg0, arg1] = msg.split(' ');
+  console.log('action: ', action);
+  console.log('arg0: ', arg0);
+  console.log('arg1: ', arg1);
+
+  if (action === '!balance') {
+    const res = await ax.get('/balance');
+    const balance = res.data;
+
+    msg.reply(`The faucet has ${balance/10**15} WNDs remaining.`, `The faucet has ${balance/10**15} OAKs remaining.`);
+  }
+
+  if (action === '!drip') {
+    try {
+      pdKeyring.decodeAddress(arg0);
+    } catch (e) {
+      msg.reply(`${sender} provided an incompatible address.`);
+      return;
+    }
+
+    let amount = 150;
+    if (sender.endsWith(':web3.foundation') && arg1) {
+      amount = arg1;
+    }
+
+    const res = await ax.post('/bot-endpoint', {
+      sender,
+      address: arg0,
+      amount,
+    });
+
+    if (res.data === 'LIMIT') {
+      msg.reply(`${sender} has reached their daily quota. Only request twice per 24 hours.`);
+      return;
+    }
+
+    msg.reply(`Sent ${sender} ${amount} mWNDs.`);
+  }
+
+  if (action === '!faucet') {
+    msg.reply(`
+Usage:
+  !balance - Get the faucet's balance.
+  !drip <Address> - Send Westend WNDs to <Address>.
+  !faucet - Prints usage information.`);
   }
 });
 
-client.login('ODY5ODk0NzQxMzI2NzA4NzY2.YQE2nA.FKZUxn1TiIi4w1E7NPmFHl7r4fQ');
+client.login(process.env.ACCESS_TOKEN);
