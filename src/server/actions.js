689e6cb38418e3ef924ba6f53608e9f4d9434ca0
imstar15 <imstar15@qq.com>
2021-07-28 15:18:54 +0800

    Add OAK Testnet config

--- src/server/actions.js
+++ src/server/actions.js
@@ -1,17 +1,19 @@
 const { WsProvider, ApiPromise } = require('@polkadot/api');
 const pdKeyring = require('@polkadot/keyring');
+const config = require('./config');
+
+const { endpoint, types } = config;
 
 class Actions {
-  async create(mnemonic, url = 'wss://westend-rpc.polkadot.io/') {
+  async create(mnemonic, url = endpoint) {
     const provider = new WsProvider(url);
-    this.api = await ApiPromise.create({ provider });
+    this.api = await ApiPromise.create({ provider, types });
     const keyring = new pdKeyring.Keyring({ type: 'sr25519' });
     this.account = keyring.addFromMnemonic(mnemonic);
   }
 
   async sendDOTs(address, amount = 150) {
-    amount = amount * 10**6;
-
+    amount = amount * 10**10;
     const transfer = this.api.tx.balances.transfer(address, amount);
     const hash = await transfer.signAndSend(this.account);
 
@@ -19,7 +21,8 @@ class Actions {
   }
 
   async checkBalance() {
-    return this.api.query.balances.freeBalance(this.account.address);
+    const { data: { free } } = await this.api.query.system.account(this.account.address);
+    return free;
   }
 }
 
