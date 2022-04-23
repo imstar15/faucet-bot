cbbe21c0c69e5b7b0f1ecdaae0b7ba789a158363
imstar15 <imstar15@qq.com>
2022-04-23 22:08:36 +0800

    GO

--- src/server/storage/database.js
+++ src/server/storage/database.js
@@ -1,30 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres',
-  // process.env.DB_USER || 'root',
-  // process.env.DB_PASSWORD || '',
-  // {
-  //   host: process.env.DB_HOST || 'localhost',
-  //   port: process.env.DB_PORT || 5432,
-  //   dialect: 'postgres',
-  // }
-  'root',
-  'Rewq1234',
+const sequelize = new Sequelize(process.env.DB_SCHEMA || 'faucet',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASSWORD || '',
   {
-    host: 'database-1.cp3zurulhfbo.us-east-1.rds.amazonaws.com',
-    port: 5432,
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
-    // dialectOptions: { ssl:'Amazon RDS' },
+    dialectOptions: { ssl:'Amazon RDS' },
   }
 );
-
-console.log('sequelize: ', sequelize);
 
 sequelize.authenticate().then(() => {
   console.log('Connection has been established successfully.');
 }).catch((error) => {
   console.error('Unable to connect to the database:', error);
-});
+})
 
 const Faucet = sequelize.define('faucet', {
   item: {
